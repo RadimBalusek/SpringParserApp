a94212426f842e74b7f29a259787db7fa1ee5303
radim <radim.balusek@seznam.cz>
2020-02-02 20:56:52 +0100

    first commit

---
+++ src/main/java/com/springframework/parserexample/parserapp/ParserappApplication.java
@@ -0,0 +1,13 @@
+package com.springframework.parserexample.parserapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParserappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParserappApplication.class, args);
+    }
+
+}
